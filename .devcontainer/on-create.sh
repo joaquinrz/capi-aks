dd6cf57fb40535f8ecf3e8c83562044497f65fd1
Joaquin Rodriguez <joaquin@outlook.com>
2022-07-15 17:09:50 +0000

    cleanup

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,14 +5,6 @@
 echo "on-create start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
 
-# clone repos
-git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
-git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
-
-# restore the repos
-dotnet restore /workspaces/webvalidate/src/webvalidate.sln
-dotnet restore /workspaces/imdb-app/src/imdb.csproj
-
 export REPO_BASE=$PWD
 export PATH="$PATH:$REPO_BASE/cli"
 
@@ -26,39 +18,13 @@ mkdir -p "$HOME/.ssh"
     echo "compinit"
 } >> "$HOME/.zshrc"
 
-# create local registry
-docker network create k3d
-k3d registry create registry.localhost --port 5500
-docker network connect k3d k3d-registry.localhost
-
-# update the base docker images
-docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:6.0
-docker pull ghcr.io/cse-labs/webv-red:latest
-
-# echo "dowloading kic CLI"
-# cd cli || exit
-# tag="0.4.3"
-# wget -O kic.tar.gz "https://github.com/retaildevcrews/akdc/releases/download/$tag/kic-$tag-linux-amd64.tar.gz"
-# tar -xvzf kic.tar.gz
-# rm kic.tar.gz
-# cd "$OLDPWD" || exit
-
 echo "generating kic completion"
 kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
 
-echo "creating k3d cluster"
-kic cluster rebuild
-
-echo "bilding IMDb"
-kic build imdb
-
-echo "building WebValidate"
-sed -i "s/RUN dotnet test//g" /workspaces/webvalidate/Dockerfile
-kic build webv
-
-echo "deploying k3d cluster"
-kic cluster deploy
+# install clusterctl
+curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.1.5/clusterctl-linux-amd64 -o clusterctl
+chmod +x ./clusterctl
+sudo mv ./clusterctl /usr/local/bin/clusterctl
 
 # only run apt upgrade on pre-build
 if [ "$CODESPACE_NAME" = "null" ]
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,9 +5,6 @@
 echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
-# update the repos
-git -C /workspaces/imdb-app pull
-git -C /workspaces/webvalidate pull
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
